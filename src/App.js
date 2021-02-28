08cf8010e6d29f8a94955bb106a95c7652d1f236
vangilvan <vangilvan2@gmail.com>
2021-02-28 01:43:30 -0300

    React SSR

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import { lazy, Suspense } from "react";
 import './App.css';
+const Slogan = lazy(() => import('./texto'));
+const  Full = lazy(() => import('./img'));
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Suspense fallback={<p>Carregando...</p>}>
+          <Full/>
+        </ Suspense>
+        <br /> <br />
+        <Suspense fallback={<p>Carregando...</p>}>
+          < Slogan />
+        </Suspense>
+
+        
       </header>
     </div>
   );
---
+++ src/texto.js
@@ -0,0 +1,10 @@
+export default function Slogan() {
+    return (
+    <p>
+        Seja bem vindo ao nosso site, 
+        aqui programadores tem desconto               
+    </p>
+    );
+}
+
+{/* Esse Texto esta com o SSR */}
